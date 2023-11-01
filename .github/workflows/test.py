37e4b1b12e091688bc81047c22dbeafa37316641
Jooeun Park <civbf1@gmail.com>
2023-10-31 20:57:29 -0700

    FID with many pics

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,50 +1,62 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+import os
+import scipy
 
-# Load InceptionV3 model pre-trained on ImageNet data and exclude top classification layer
 model = InceptionV3(include_top=False, pooling='avg', input_shape=(299, 299, 3))
 
-def calculate_fid(img1, img2):
-    # Ensure images are of the shape (299, 299, 3)
-    img1 = tf.image.resize(img1, (299, 299))
-    img2 = tf.image.resize(img2, (299, 299))
+
+def load_img(folder):
+    print("Loading images")
+    images = []
+    for filename in os.listdir(folder):
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            img_path = os.path.join(folder, filename)
+            img = tf.keras.preprocessing.image.load_img(img_path, target_size=(299, 299))
+            img_array = np.array(img)
+            images.append(img_array)
+    return np.array(images)
+
+
+def calculate_fid(img1, img2):  # Ensure images are of the shape (299, 299, 3)
 
     # Preprocess images (scaling and other operations)
-    img1 = preprocess_input(img1)
-    img2 = preprocess_input(img2)
+    images1 = preprocess_input(img1)
+    images2 = preprocess_input(img2)
 
-    print("Preprecessing images")
+
+    print("Preprocessing images")
 
     # Calculate activations
-    act1 = model(img1)
-    act2 = model(img2)
+    act1 = model(images1)
+    act2 = model(images2)
 
-    # Calculate mean and variance statistics
-    mu1, sigma1_sq = act1.numpy().mean(), act1.numpy().var()
-    mu2, sigma2_sq = act2.numpy().mean(), act2.numpy().var()
+    # Calculate mean and covariance statistics
+    mu1, sigma1 = np.mean(act1, axis=0), np.cov(act1, rowvar=False)
+    mu2, sigma2 = np.mean(act2, axis=0), np.cov(act2, rowvar=False)
 
     # Calculate sum squared difference between means
-    ssdiff = np.sum((mu1 - mu2)**2.0)
+    ssdiff = np.sum((mu1 - mu2) ** 2.0)
 
-    fid = ssdiff + sigma1_sq + sigma2_sq - 2.0 * np.sqrt(sigma1_sq * sigma2_sq)
+    # Calculate sqrt of product between cov
+    covmean = scipy.linalg.sqrtm(sigma1.dot(sigma2))
+
+    # Check and correct imaginary numbers from sqrt
+    if np.iscomplexobj(covmean):
+        covmean = covmean.real
+
+    fid = ssdiff + np.trace(sigma1 + sigma2 - 2.0 * covmean)
 
     return fid
 
-# Load your images (replace with your paths or image loading method)
-real_image = tf.keras.preprocessing.image.load_img('real.png', target_size=(299, 299))
-real_image = np.array(real_image)
 
-generated_image = tf.keras.preprocessing.image.load_img('test.png', target_size=(299, 299))
-generated_image = np.array(generated_image)
-
-# Images should be in the shape (N, 299, 299, 3), so we need to expand dimensions
-real_image = np.expand_dims(real_image, axis=0)
-generated_image = np.expand_dims(generated_image, axis=0)
+generated_img = load_img('generated_img')
+real_img = load_img('real_img')
 
 # Calculate FID
 print("Calculating fid")
-fid_value = calculate_fid(real_image, generated_image)
+fid_value = calculate_fid(real_img, generated_img)
 print(f"FID: {fid_value}")
 
-assert fid_value<1 == True;
+assert fid_value < 1 == True;
